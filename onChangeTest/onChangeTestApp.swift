df84b8735665d1753369921999d98202e69b2145
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-24 17:29:26 +0700

    Initial Commit

---
+++ onChangeTest/onChangeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  onChangeTestApp.swift
+//  onChangeTest
+//
+//  Created by emi oiso on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct onChangeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
